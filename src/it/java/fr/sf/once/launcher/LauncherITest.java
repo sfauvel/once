e400d3c6480e3a40449758c9d367b72517545076
Sébastien Fauvel <sebastien.fauvel@orange.com>
2014-02-12 00:31:59 +0100

    First complete fonctional test

--- src/it/java/fr/sf/once/launcher/LauncherITest.java
+++ src/it/java/fr/sf/once/launcher/LauncherITest.java
@@ -1,18 +1,41 @@
 package fr.sf.once.launcher;
 
-import static org.junit.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
 
+import java.io.StringWriter;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.WriterAppender;
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.sf.once.comparator.ComparateurAvecSubstitutionEtType;
+import fr.sf.once.report.Reporting;
 
 public class LauncherITest {
+    private StringWriter writer = new StringWriter();
+
+    @Before
+    public void setReporter() {
+        Reporting.LOG_RESULTAT.addAppender(new WriterAppender(new PatternLayout("%m\n"), writer));
+        Reporting.LOG_RESULTAT.setLevel(Level.INFO);
+    }
+    
     @Test
-    public void testName() throws Exception {
+    public void make_a_full_execution() throws Exception {
+
         Launcher launcher = new Launcher()
                 .withSource("src/test/resources/exemple", "UTF-8")
                 .withComparator(ComparateurAvecSubstitutionEtType.class)
                 .withMinimalSize(10);
+        
         launcher.execute();
+
+        assertThat(writer.toString().split("\n")).isEqualTo(new String[] {
+                "Tokens number:63 Duplications number:2 Substitutions number:0",
+                "  100% 7/7 lines fr.sf.once.CopieMethode.firstMethod 7:51 <-> 14:6",
+                "  100% 7/7 lines fr.sf.once.CopieMethode.secondMethod 16:53 <-> 23:6" });
     }
+    
 }
--- src/test/java/fr/sf/commons/FilesTest.java
+++ src/test/java/fr/sf/commons/FilesTest.java
@@ -4,12 +4,18 @@ import static org.junit.Assert.*;
 
 import java.io.File;
 
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.PatternLayout;
 import org.junit.Test;
+
+import fr.sf.once.core.ManagerToken;
+import fr.sf.once.report.Reporting;
 
 public class FilesTest {
 
     @Test
-    public void when_file_does_not_exist_an_excpetion_is_thrown_____() throws Exception {
+    public void when_file_does_not_exist_an_excpetion_is_thrown() throws Exception {
         try {
             Files.visitFile("/NoExistingFolder", new Files.FileVisitor() {
                 @Override
