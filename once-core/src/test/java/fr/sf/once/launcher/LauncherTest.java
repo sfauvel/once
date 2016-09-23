925464dea58e0e5e71d6eec8c0a451f354a91113
FAUVEL Sebastien <sebastien.fauvel@orange.com>
2016-09-24 00:11:23 +0200

    Refactor test

--- once-core/src/test/java/fr/sf/once/launcher/LauncherTest.java
+++ once-core/src/test/java/fr/sf/once/launcher/LauncherTest.java
@@ -1,6 +1,8 @@
 package fr.sf.once.launcher;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.assertj.core.api.Assertions;
@@ -16,47 +18,47 @@ public class LauncherTest {
 	
 	@Test
 	public void should_use_default_source_directory_when_no_args() throws Exception {
-		File propertiesFile = temporaryFolder.newFile("tmp.properties");
-		String[] args = new String[] {};
-		
-		OnceProperties properties = Launcher.loadConfiguration(args, propertiesFile);
+		OnceProperties properties = Launcher.loadConfiguration(
+				new String[] {}, 
+				createFile());
 
 		Assertions.assertThat(properties.getSourceDir()).isEqualTo(OnceProperties.DEFAULT_SOURCE_DIR);
 	}
 
 	@Test
 	public void should_use_source_directory_given_into_parameters() throws Exception {
-		File propertiesFile = temporaryFolder.newFile("tmp.properties");
-		String[] args = new String[] {"./src"};
-		
-		OnceProperties properties = Launcher.loadConfiguration(args, propertiesFile);
+		OnceProperties properties = Launcher.loadConfiguration(
+				new String[] {"./src"},
+				createFile());
 		
 		Assertions.assertThat(properties.getSourceDir()).isEqualTo("./src");
 	}
 	
 	@Test
 	public void should_use_source_directory_given_into_properties_file_when_no_parameter() throws Exception {
-		File propertiesFile = temporaryFolder.newFile("tmp.properties");
-		PrintWriter printWriter = new PrintWriter(propertiesFile);
-		printWriter.format("%s=%s", OnceProperties.SRC_DIR, "./src");
-		printWriter.close();
-		String[] args = new String[] {};
-		
-		OnceProperties properties = Launcher.loadConfiguration(args, propertiesFile);
+		OnceProperties properties = Launcher.loadConfiguration(
+				new String[] {}, 
+				createFile(OnceProperties.SRC_DIR + "=./src"));
 		
 		Assertions.assertThat(properties.getSourceDir()).isEqualTo("./src");
 	}
-	
+
 	@Test
 	public void should_use_source_directory_given_into_parameters_behind_this_one_in_properties_file() throws Exception {
-		File propertiesFile = temporaryFolder.newFile("tmp.properties");
-		PrintWriter printWriter = new PrintWriter(propertiesFile);
-		printWriter.format("%s=%s", OnceProperties.SRC_DIR, "./src");
-		printWriter.close();
-		String[] args = new String[] {"./argSrc"};
-		
-		OnceProperties properties = Launcher.loadConfiguration(args, propertiesFile);
+		OnceProperties properties = Launcher.loadConfiguration(
+				new String[] {"./argSrc"}, 
+				createFile());
 		
 		Assertions.assertThat(properties.getSourceDir()).isEqualTo("./argSrc");
+	}
+	
+	private File createFile(String... properties) throws IOException, FileNotFoundException {
+		File propertiesFile = temporaryFolder.newFile("tmp.properties");
+		PrintWriter printWriter = new PrintWriter(propertiesFile);
+		for (String property : properties) {			
+			printWriter.format(property);
+		}
+		printWriter.close();
+		return propertiesFile;
 	}
 }
